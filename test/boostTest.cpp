e80673bdccfea3749dad6e907a9f29d2e68a1d3a
Alex Beregszaszi <alex@rtfs.hu>
2018-08-01 22:37:46 +0100

    Move SMTChecker tests from C++ to isoltest

--- test/boostTest.cpp
+++ test/boostTest.cpp
@@ -155,8 +155,6 @@ test_suite* init_unit_test_suite( int /*argc*/, char* /*argv*/[] )
 		})
 			removeTestSuite(suite);
 	}
-	if (dev::test::Options::get().disableSMT)
-		removeTestSuite("SMTChecker");
 
 	return 0;
 }
